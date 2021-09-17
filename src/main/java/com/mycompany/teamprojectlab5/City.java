75240657a6041564da2596dbf43e7b52fd982f32
Alejandro Rivas <alejandro.rivas@go.tarleton.edu>
2021-09-16 21:30:57 -0500

    FileInput added

--- src/main/java/com/mycompany/teamprojectlab5/City.java
+++ src/main/java/com/mycompany/teamprojectlab5/City.java
@@ -45,5 +45,5 @@ public class City {
             return false;
         }
         return true;
-    }
+    } 
 }  
--- src/main/java/com/mycompany/teamprojectlab5/Main.java
+++ src/main/java/com/mycompany/teamprojectlab5/Main.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
- 
+  
 
 public class Main {
     public static void main(String[] args) {
